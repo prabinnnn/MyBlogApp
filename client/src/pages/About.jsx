58ced359cba6dc132f8f138df7d166120821994d
prabinnnn <prabinbhandari120@gmail.com>
2024-06-19 14:03:03 +0545

    blogs

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-page">
+      <div className="container">
+        <h2 className="page-title">About Us</h2>
+        <div className="about-content">
+          <p>
+            Welcome to our blog! We are passionate about sharing knowledge and
+            insights on various topics related to [your blogs focus]. Our goal
+            is to provide valuable information to our readers in an engaging
+            and informative way.
+          </p>
+          <p>
+            Whether youre interested in [specific topics your blog covers],
+            looking for [specific types of content], or simply browsing for
+            inspiration, we hope you find our blog useful and enjoyable.
+          </p>
+          <p>
+            Feel free to explore our articles and leave comments to join the
+            conversation. If you have any questions or suggestions, please
+            dont hesitate to <a href="/contact">contact us</a>. Wed love to
+            hear from you!
+          </p>
+          <p>Thank you for visiting!</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { Box, styled, Typography, Link } from '@mui/material';
+import { Email } from '@mui/icons-material';
 
-const Contact = () => {
-  return <div>Contact</div>;
-};
+const Banner = styled(Box)`
+  background-image: url(http://mrtaba.ir/image/bg2.jpg);
+  width: 100%;
+  height: 50vh;
+  background-position: left 0px top -100px;
+  background-size: cover;
+`;
+
+const Wrapper = styled(Box)`
+  padding: 20px;
+  & > h3,
+  & > h5 {
+    margin-top: 50px;
+  }
+`;
+
+const Text = styled(Typography)`
+  color: #878787;
+`;
+
+function Contact() {
+  return (
+    <Box>
+      <Banner />
+      <Wrapper>
+        <Typography variant="h3">Getting in touch is easy!</Typography>
+        <Text variant="h5">
+          Send me an Email
+          <Link
+            href="mailto:bhandariprabin402@gmail.com?Subject=This is a subject"
+            target="_blank"
+            color="inherit"
+          >
+            <Email />
+          </Link>
+          .
+        </Text>
+      </Wrapper>
+    </Box>
+  );
+}
 
 export default Contact;
